4c276c1d680ba0768fbe5f29f00b5e644c01b3a8
Edgar S Fernandes <fernandes.s.edgar@gmail.com>
2016-12-21 17:43:57 -0200

    Fixed UI testing

--- app/src/androidTest/java/br/com/edsilfer/android/sinterface/demo/UITestSearchSamples.kt
+++ app/src/androidTest/java/br/com/edsilfer/android/sinterface/demo/UITestSearchSamples.kt
@@ -1,42 +1,35 @@
 package br.com.edsilfer.android.sinterface.demo
 
-import android.app.Instrumentation
 import android.os.Environment
 import android.support.test.InstrumentationRegistry.getInstrumentation
 import android.support.test.espresso.Espresso
-import android.support.test.espresso.Espresso.closeSoftKeyboard
 import android.support.test.espresso.Espresso.onView
-import android.support.test.espresso.IdlingPolicies
+import android.support.test.espresso.NoActivityResumedException
+import android.support.test.espresso.UiController
+import android.support.test.espresso.ViewAction
 import android.support.test.espresso.action.ViewActions.click
 import android.support.test.espresso.action.ViewActions.typeText
+import android.support.test.espresso.matcher.ViewMatchers.isRoot
 import android.support.test.espresso.matcher.ViewMatchers.withId
 import android.support.test.filters.LargeTest
 import android.support.test.rule.ActivityTestRule
 import android.support.test.runner.AndroidJUnit4
-import android.support.v7.app.AppCompatActivity
-import android.util.Log
-import br.com.edsilfer.android.search_interface.presenter.activity.ActivitySearch
-import br.com.edsilfer.android.sinterface.demo.presenter.ActivityHomepage
-import br.com.edsilfer.android.sinterface.demo.util.DisableAnimationsRule
-import br.com.edsilfer.android.sinterface.demo.util.ElapsedTimeIdlingResource
-import br.com.edsilfer.android.sinterface.demo.util.TestCommunicationManager
-import br.com.edsilfer.android.sinterface.demo.util.TestPauseManager
-import br.com.edsilfer.kotlin_support.extensions.sendEmail
-import br.com.edsilfer.kotlin_support.extensions.takeScreenShot
-import br.com.edsilfer.kotlin_support.model.DirectoryPath
-import br.com.edsilfer.kotlin_support.model.Email
-import br.com.edsilfer.kotlin_support.model.Sender
-import org.apache.commons.io.FileUtils
-import org.junit.*
-import org.junit.runner.RunWith
-import java.io.File
-import java.text.SimpleDateFormat
-import java.util.*
-import java.util.concurrent.TimeUnit
-import android.app.Activity
-import android.support.test.InstrumentationRegistry.getInstrumentation
 import android.support.test.runner.lifecycle.ActivityLifecycleMonitorRegistry
 import android.support.test.runner.lifecycle.Stage
+import android.support.v7.app.AppCompatActivity
+import android.util.Log
+import android.view.View
+import br.com.edsilfer.android.sinterface.demo.presenter.ActivityHomepage
+import br.com.edsilfer.android.sinterface.demo.util.TestCommunicationManager
+import br.com.edsilfer.kotlin_support.extensions.takeScreenShot
+import br.com.edsilfer.kotlin_support.model.DirectoryPath
+import org.apache.commons.io.FileUtils
+import org.hamcrest.Matcher
+import org.junit.After
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import java.io.File
 
 
 /**
@@ -49,17 +42,51 @@ class UITestSearchSamples {
 
     companion object {
         private val SCREENSHOTS_DIRECTORY = "search-interface"
-        private val TIME_OUT = 5000L
-        @get:ClassRule
-        var disableAnimationsRule = DisableAnimationsRule()
+        private val NUMBER_OF_BACKS = 2
+        private val WAITING_TIME = 1500L
     }
 
     private var finished = false
-
-    @get:Rule
-    var mActivityRule = ActivityTestRule(ActivityHomepage::class.java)
     private var mCurrentActivity: AppCompatActivity? = null
 
+    @get:Rule
+    var mActivityRule = ActivityTestRule(ActivityHomepage::class.java, false, true)
+
+    @Test
+    fun launchSample01() {
+        onView(withId(R.id.btn_sample_01)).perform(click())
+        onView(withId(R.id.input)).perform(typeText("Diana"))
+        waitFor(WAITING_TIME)
+    }
+
+    @Test
+    fun launchSample02() {
+        onView(withId(R.id.btn_sample_02)).perform(click())
+        onView(withId(R.id.input)).perform(typeText("Clark"))
+    }
+
+    @Test
+    fun launchSample03() {
+        onView(withId(R.id.btn_sample_03)).perform(click())
+        onView(withId(R.id.input)).perform(typeText("Diana"))
+        onView(withId(R.id.wrapper)).perform(click())
+        onView(withId(R.id.input)).perform(typeText("a"))
+        waitFor(WAITING_TIME)
+        finished = true
+    }
+
+    @After
+    fun tearDown() {
+        getActivity().takeScreenShot(location = DirectoryPath.EXTERNAL, path = SCREENSHOTS_DIRECTORY, openScreenShot = false, showToast = false)
+        waitFor(WAITING_TIME)
+        if (finished) {
+            TestCommunicationManager.sendNotificationEmail(mActivityRule.activity)
+            FileUtils.cleanDirectory(File("${Environment.getExternalStorageDirectory()}/$SCREENSHOTS_DIRECTORY"))
+        }
+        closeActivity()
+    }
+
+    // UTILITY =====================================================================================
     private fun getActivity(): AppCompatActivity {
         getInstrumentation().runOnMainSync {
             val resumedActivity = ActivityLifecycleMonitorRegistry.getInstance().getActivitiesInStage(Stage.RESUMED)
@@ -71,43 +98,37 @@ class UITestSearchSamples {
         return mCurrentActivity!!
     }
 
-    // BEFORE --------------------------------------------------------------------------------------
-    @Before
-    fun setup() {
-        TestPauseManager.setWaitingPolice()
+    /**
+     * Required to close the activity when test is finishing, allowing the execution of the repetition behavior
+     */
+    private fun closeActivity() {
+        try {
+            for (i in 0..NUMBER_OF_BACKS - 1) {
+                Espresso.pressBack()
+            }
+        } catch (e: NoActivityResumedException) {
+            Log.e(UITestSearchSamples::class.java.simpleName, "Unable to close activity", e)
+        }
+
     }
 
-    // TESTS ---------------------------------------------------------------------------------------
-    @Test
-    fun launchSample01() {
-        onView(withId(R.id.btn_sample_01)).perform(click())
-        onView(withId(R.id.input)).perform(typeText("Diana"))
-        getActivity().takeScreenShot(location = DirectoryPath.EXTERNAL, path = SCREENSHOTS_DIRECTORY, openScreenShot = false, showToast = false)
-    }
+    /**
+     * Perform action of waiting for a specific time.
+     */
+    fun waitFor(millis: Long): ViewAction {
+        return object : ViewAction {
+            override fun getConstraints(): Matcher<View> {
+                return isRoot()
+            }
 
-    @Test
-    fun launchSample02() {
-        onView(withId(R.id.btn_sample_02)).perform(click())
-        onView(withId(R.id.input)).perform(typeText("Clark"))
-        getActivity().takeScreenShot(location = DirectoryPath.EXTERNAL, path = SCREENSHOTS_DIRECTORY, openScreenShot = false, showToast = false)
-        Thread.sleep(500)
-        getActivity().finish()
-    }
+            override fun getDescription(): String {
+                return "Wait for $millis milliseconds."
+            }
 
-    @Test
-    fun launchSample03() {
-        onView(withId(R.id.btn_sample_03)).perform(click())
-        onView(withId(R.id.input)).perform(typeText("Diana"))
-        onView(withId(R.id.wrapper)).perform(click())
-        onView(withId(R.id.input)).perform(typeText("a"))
-        getActivity().takeScreenShot(location = DirectoryPath.EXTERNAL, path = SCREENSHOTS_DIRECTORY, openScreenShot = false, showToast = false)
-        finished = true
-    }
-
-    // AFTER ---------------------------------------------------------------------------------------
-    @After
-    fun tearDown() {
-        if (finished) TestCommunicationManager.sendNotificationEmail(getActivity())
+            override fun perform(uiController: UiController, view: View) {
+                uiController.loopMainThreadForAtLeast(millis)
+            }
+        }
     }
 }
 
